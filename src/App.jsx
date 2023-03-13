27ee0dc41634a04fbe32285553bfa92a50dfc630
Jek-San <88803418+Jek-San@users.noreply.github.com>
2023-03-13 16:40:42 +0700

    monday

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,174 @@ function App() {
   return (
     <div>
       <Navbar />
+      <div className="text">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
+        nihil fuga repellendus adipisci, odit, iure aspernatur earum,
+        consequuntur quam tempore laboriosam. Qui aliquam harum unde fugiat
+        sequi a nam perspiciatis, nihil, error suscipit cupiditate dignissimos
+        dolores exercitationem consectetur, illo cum officiis impedit. Vel,
+        officiis fugit! Aliquam doloremque facilis asperiores ea numquam,
+        mollitia est veniam eos et voluptatibus soluta ipsa cum, dolorem,
+        quaerat exercitationem vel pariatur nostrum. Dolorum quisquam
+        consectetur beatae, reiciendis architecto eligendi tempore doloribus qui
+        fugit consequatur nemo deleniti corporis culpa fuga, soluta tempora ab
+        blanditiis sunt debitis error incidunt, expedita omnis. Hic consectetur,
+        qui recusandae repellendus blanditiis minus.
+      </div>
+      <div className="text">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
+        nihil fuga repellendus adipisci, odit, iure aspernatur earum,
+        consequuntur quam tempore laboriosam. Qui aliquam harum unde fugiat
+        sequi a nam perspiciatis, nihil, error suscipit cupiditate dignissimos
+        dolores exercitationem consectetur, illo cum officiis impedit. Vel,
+        officiis fugit! Aliquam doloremque facilis asperiores ea numquam,
+        mollitia est veniam eos et voluptatibus soluta ipsa cum, dolorem,
+        quaerat exercitationem vel pariatur nostrum. Dolorum quisquam
+        consectetur beatae, reiciendis architecto eligendi tempore doloribus qui
+        fugit consequatur nemo deleniti corporis culpa fuga, soluta tempora ab
+        blanditiis sunt debitis error incidunt, expedita omnis. Hic consectetur,
+        qui recusandae repellendus blanditiis minus.
+        <div className="text">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
+          nihil fuga repellendus adipisci, odit, iure aspernatur earum,
+          consequuntur quam tempore laboriosam. Qui aliquam harum unde fugiat
+          sequi a nam perspiciatis, nihil, error suscipit cupiditate dignissimos
+          dolores exercitationem consectetur, illo cum officiis impedit. Vel,
+          officiis fugit! Aliquam doloremque facilis asperiores ea numquam,
+          mollitia est veniam eos et voluptatibus soluta ipsa cum, dolorem,
+          quaerat exercitationem vel pariatur nostrum. Dolorum quisquam
+          consectetur beatae, reiciendis architecto eligendi tempore doloribus
+          qui fugit consequatur nemo deleniti corporis culpa fuga, soluta
+          tempora ab blanditiis sunt debitis error incidunt, expedita omnis. Hic
+          consectetur, qui recusandae repellendus blanditiis minus.
+        </div>
+        <div className="text">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
+          nihil fuga repellendus adipisci, odit, iure aspernatur earum,
+          consequuntur quam tempore laboriosam. Qui aliquam harum unde fugiat
+          sequi a nam perspiciatis, nihil, error suscipit cupiditate dignissimos
+          dolores exercitationem consectetur, illo cum officiis impedit. Vel,
+          officiis fugit! Aliquam doloremque facilis asperiores ea numquam,
+          mollitia est veniam eos et voluptatibus soluta ipsa cum, dolorem,
+          quaerat exercitationem vel pariatur nostrum. Dolorum quisquam
+          consectetur beatae, reiciendis architecto eligendi tempore doloribus
+          qui fugit consequatur nemo deleniti corporis culpa fuga, soluta
+          tempora ab blanditiis sunt debitis error incidunt, expedita omnis. Hic
+          consectetur, qui recusandae repellendus blanditiis minus.
+        </div>
+        <div className="text">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
+          nihil fuga repellendus adipisci, odit, iure aspernatur earum,
+          consequuntur quam tempore laboriosam. Qui aliquam harum unde fugiat
+          sequi a nam perspiciatis, nihil, error suscipit cupiditate dignissimos
+          dolores exercitationem consectetur, illo cum officiis impedit. Vel,
+          officiis fugit! Aliquam doloremque facilis asperiores ea numquam,
+          mollitia est veniam eos et voluptatibus soluta ipsa cum, dolorem,
+          quaerat exercitationem vel pariatur nostrum. Dolorum quisquam
+          consectetur beatae, reiciendis architecto eligendi tempore doloribus
+          qui fugit consequatur nemo deleniti corporis culpa fuga, soluta
+          tempora ab blanditiis sunt debitis error incidunt, expedita omnis. Hic
+          consectetur, qui recusandae repellendus blanditiis minus.
+        </div>
+        <div className="text">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
+          nihil fuga repellendus adipisci, odit, iure aspernatur earum,
+          consequuntur quam tempore laboriosam. Qui aliquam harum unde fugiat
+          sequi a nam perspiciatis, nihil, error suscipit cupiditate dignissimos
+          dolores exercitationem consectetur, illo cum officiis impedit. Vel,
+          officiis fugit! Aliquam doloremque facilis asperiores ea numquam,
+          mollitia est veniam eos et voluptatibus soluta ipsa cum, dolorem,
+          quaerat exercitationem vel pariatur nostrum. Dolorum quisquam
+          consectetur beatae, reiciendis architecto eligendi tempore doloribus
+          qui fugit consequatur nemo deleniti corporis culpa fuga, soluta
+          tempora ab blanditiis sunt debitis error incidunt, expedita omnis. Hic
+          consectetur, qui recusandae repellendus blanditiis minus.
+        </div>
+        <div className="text">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
+          nihil fuga repellendus adipisci, odit, iure aspernatur earum,
+          consequuntur quam tempore laboriosam. Qui aliquam harum unde fugiat
+          sequi a nam perspiciatis, nihil, error suscipit cupiditate dignissimos
+          dolores exercitationem consectetur, illo cum officiis impedit. Vel,
+          officiis fugit! Aliquam doloremque facilis asperiores ea numquam,
+          mollitia est veniam eos et voluptatibus soluta ipsa cum, dolorem,
+          quaerat exercitationem vel pariatur nostrum. Dolorum quisquam
+          consectetur beatae, reiciendis architecto eligendi tempore doloribus
+          qui fugit consequatur nemo deleniti corporis culpa fuga, soluta
+          tempora ab blanditiis sunt debitis error incidunt, expedita omnis. Hic
+          consectetur, qui recusandae repellendus blanditiis minus.
+        </div>
+        <div className="text">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
+          nihil fuga repellendus adipisci, odit, iure aspernatur earum,
+          consequuntur quam tempore laboriosam. Qui aliquam harum unde fugiat
+          sequi a nam perspiciatis, nihil, error suscipit cupiditate dignissimos
+          dolores exercitationem consectetur, illo cum officiis impedit. Vel,
+          officiis fugit! Aliquam doloremque facilis asperiores ea numquam,
+          mollitia est veniam eos et voluptatibus soluta ipsa cum, dolorem,
+          quaerat exercitationem vel pariatur nostrum. Dolorum quisquam
+          consectetur beatae, reiciendis architecto eligendi tempore doloribus
+          qui fugit consequatur nemo deleniti corporis culpa fuga, soluta
+          tempora ab blanditiis sunt debitis error incidunt, expedita omnis. Hic
+          consectetur, qui recusandae repellendus blanditiis minus.
+        </div>
+        <div className="text">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
+          nihil fuga repellendus adipisci, odit, iure aspernatur earum,
+          consequuntur quam tempore laboriosam. Qui aliquam harum unde fugiat
+          sequi a nam perspiciatis, nihil, error suscipit cupiditate dignissimos
+          dolores exercitationem consectetur, illo cum officiis impedit. Vel,
+          officiis fugit! Aliquam doloremque facilis asperiores ea numquam,
+          mollitia est veniam eos et voluptatibus soluta ipsa cum, dolorem,
+          quaerat exercitationem vel pariatur nostrum. Dolorum quisquam
+          consectetur beatae, reiciendis architecto eligendi tempore doloribus
+          qui fugit consequatur nemo deleniti corporis culpa fuga, soluta
+          tempora ab blanditiis sunt debitis error incidunt, expedita omnis. Hic
+          consectetur, qui recusandae repellendus blanditiis minus.
+        </div>
+        <div className="text">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
+          nihil fuga repellendus adipisci, odit, iure aspernatur earum,
+          consequuntur quam tempore laboriosam. Qui aliquam harum unde fugiat
+          sequi a nam perspiciatis, nihil, error suscipit cupiditate dignissimos
+          dolores exercitationem consectetur, illo cum officiis impedit. Vel,
+          officiis fugit! Aliquam doloremque facilis asperiores ea numquam,
+          mollitia est veniam eos et voluptatibus soluta ipsa cum, dolorem,
+          quaerat exercitationem vel pariatur nostrum. Dolorum quisquam
+          consectetur beatae, reiciendis architecto eligendi tempore doloribus
+          qui fugit consequatur nemo deleniti corporis culpa fuga, soluta
+          tempora ab blanditiis sunt debitis error incidunt, expedita omnis. Hic
+          consectetur, qui recusandae repellendus blanditiis minus.
+        </div>
+        <div className="text">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
+          nihil fuga repellendus adipisci, odit, iure aspernatur earum,
+          consequuntur quam tempore laboriosam. Qui aliquam harum unde fugiat
+          sequi a nam perspiciatis, nihil, error suscipit cupiditate dignissimos
+          dolores exercitationem consectetur, illo cum officiis impedit. Vel,
+          officiis fugit! Aliquam doloremque facilis asperiores ea numquam,
+          mollitia est veniam eos et voluptatibus soluta ipsa cum, dolorem,
+          quaerat exercitationem vel pariatur nostrum. Dolorum quisquam
+          consectetur beatae, reiciendis architecto eligendi tempore doloribus
+          qui fugit consequatur nemo deleniti corporis culpa fuga, soluta
+          tempora ab blanditiis sunt debitis error incidunt, expedita omnis. Hic
+          consectetur, qui recusandae repellendus blanditiis minus.
+        </div>
+        <div className="text">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
+          nihil fuga repellendus adipisci, odit, iure aspernatur earum,
+          consequuntur quam tempore laboriosam. Qui aliquam harum unde fugiat
+          sequi a nam perspiciatis, nihil, error suscipit cupiditate dignissimos
+          dolores exercitationem consectetur, illo cum officiis impedit. Vel,
+          officiis fugit! Aliquam doloremque facilis asperiores ea numquam,
+          mollitia est veniam eos et voluptatibus soluta ipsa cum, dolorem,
+          quaerat exercitationem vel pariatur nostrum. Dolorum quisquam
+          consectetur beatae, reiciendis architecto eligendi tempore doloribus
+          qui fugit consequatur nemo deleniti corporis culpa fuga, soluta
+          tempora ab blanditiis sunt debitis error incidunt, expedita omnis. Hic
+          consectetur, qui recusandae repellendus blanditiis minus.
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,17 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import "./Navbar.scss"
 export const Navbar = () => {
   const [active, setActive] = useState(false)
+  const isActive = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false)
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", isActive)
+    return window.removeEventListener("scroll", isActive())
+  }, [])
   return (
-    <div className={active ? "navbar" : "navbar active"}>
+    <div className={active ? "navbar active" : "navbar"}>
       <div className="container">
         <div className="logo">
           <span className="text">fiverr</span>
@@ -17,12 +25,16 @@ export const Navbar = () => {
           <button>Join</button>
         </div>
       </div>
-      <hr />
-      <div className="menu">
-        <span>Graphics & Design</span>
-        <span>Digital Marketing</span>
-        <span>Writing & Transalation</span>
-      </div>
+      {active && (
+        <>
+          <hr />
+          <div className="menu">
+            <span>Graphics & Design</span>
+            <span>Digital Marketing</span>
+            <span>Writing & Transalation</span>
+          </div>
+        </>
+      )}
     </div>
   )
 }
